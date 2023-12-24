8d775a33e13092f7e50aec03ca1b735713a08451
AnriBeridzee <aberidze02@gmail.com>
2023-12-25 02:09:11 +0400

    Initial Commit

---
+++ test_repo/test_repoApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_repoApp.swift
+//  test_repo
+//
+//  Created by Macbook Air 13 on 25.12.23.
+//
+
+import SwiftUI
+
+@main
+struct test_repoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
